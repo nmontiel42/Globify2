9bb7342db9f70bcc45b532fc6c52c5406292aba6
Paula <pausanpi1606@gmail.com>
2024-11-26 11:46:17 +0100

    update

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -54,15 +54,18 @@ const UserProfile: React.FC = () => {
     <body>
       <header>
 		<div className='header-profile'>
-			<button>
-				<img src={logoSpotify} alt="Spotify Logo" className="spotify-logo"></img>
-			</button>
+			<div className='btns-home'>
+				<button>
+					<img src={logoSpotify} alt="Spotify Logo" className="spotify-logo"></img>
+				</button>
 
-			<button className="homeButton">
-				<svg width="28" height="28" viewBox="0 0 24 24" fill="currentColor" className='home-logo'>
-					<path d="M12.5 3.247a1 1 0 0 0-1 0L4 7.577V20h4.5v-6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v6H20V7.577l-7.5-4.33zm-2-1.732a3 3 0 0 1 3 0l7.5 4.33a2 2 0 0 1 1 1.732V21a1 1 0 0 1-1 1h-6.5a1 1 0 0 1-1-1v-6h-3v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.577a2 2 0 0 1 1-1.732l7.5-4.33z"/>
-				</svg>
-			</button>
+				<button className="homeButton">
+					<svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" className='home-logo'>
+						<path d="M12.5 3.247a1 1 0 0 0-1 0L4 7.577V20h4.5v-6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v6H20V7.577l-7.5-4.33zm-2-1.732a3 3 0 0 1 3 0l7.5 4.33a2 2 0 0 1 1 1.732V21a1 1 0 0 1-1 1h-6.5a1 1 0 0 1-1-1v-6h-3v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.577a2 2 0 0 1 1-1.732l7.5-4.33z"/>
+					</svg>
+				</button>
+			</div>
+						
 
 			<SearchBar onSongSelect={function (uri: string): void {
 				throw new Error('Function not implemented.');
